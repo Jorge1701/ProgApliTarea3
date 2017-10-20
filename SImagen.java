e924f6f094beb596893586256892f96ac8278017
Diego <Diego@Diego-pc>
2017-10-20 16:08:33 -0300

    WS IMAGENES

--- SImagen.java
+++ SImagen.java
@@ -1,5 +1,8 @@
 package servlets;
 
+import Proxy.imagen.IOException_Exception;
+import Proxy.imagen.PImagen;
+import Proxy.imagen.PImagenService;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -8,14 +11,11 @@ import java.io.OutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
-import javax.imageio.stream.MemoryCacheImageOutputStream;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import servicios.PImagen;
-import servicios.PImagenService;
 
 @WebServlet(name = "SImagen", urlPatterns = {"/SImagen"})
 public class SImagen extends HttpServlet {
@@ -26,55 +26,52 @@ public class SImagen extends HttpServlet {
             request.getRequestDispatcher("vistas/pagina_error.jsp").forward(request, response);
             return;
         }
+        
         byte[] img = null;
         String ruta = getServletContext().getRealPath("/");
-        String[] parte = ruta.split("ProgApliTarea3");
+        String[] parte = ruta.split("ProgApliTarea2");
         String tarea1 = parte[0] + "ProgApliTarea1" + File.separator;
         
         PImagenService serviceImg = new PImagenService();
         PImagen port = serviceImg.getPImagenPort();
 
         if (request.getParameter("usuario") != null) {
-            BufferedImage bi = null;
-            OutputStream out = response.getOutputStream();
+           BufferedImage bi = null;
             try {
                 //bi = ImageIO.read(new File(tarea1 + "Recursos/Imagenes/Usuarios/" + request.getParameter("usuario")));
-                img = port.getFile("Usuario", request.getParameter("usuario"));
-                out.write(img);                
-                //bi = ImageIO.read(new ByteArrayInputStream(img));
-            } catch (Exception e) {
+                img = port.getFile(tarea1 + "Recursos/Imagenes/Usuarios/", request.getParameter("usuario"));
+                bi = ImageIO.read(new ByteArrayInputStream(img));
+               
+            } catch (IOException e) {
                 bi = ImageIO.read(new File(tarea1 + "Recursos/Imagenes/Usuarios/userDefaullt.png"));
-                ImageIO.write(bi, "png", out);
-            }      
-            
+                
+            } 
+            OutputStream out = response.getOutputStream();
+            ImageIO.write(bi, "png", out);
             out.close();
         }
         if (request.getParameter("album") != null) {
             BufferedImage bi = null;
-            OutputStream out = response.getOutputStream();
             try {
                 //bi = ImageIO.read(new File(tarea1 + "Recursos/Imagenes/Albumes/" + request.getParameter("album")));
-                 img = port.getFile("Album", request.getParameter("album"));
-                 out.write(img);                
-            } catch (Exception e) {
+                img = port.getFile(tarea1 + "Recursos/Imagenes/Albumes/", request.getParameter("album"));
+                bi = ImageIO.read(new ByteArrayInputStream(img));
+            } catch (IOException e) {
                 bi = ImageIO.read(new File(tarea1 + "Recursos/Imagenes/Albumes/albumDefault.png"));
-                ImageIO.write(bi, "png", out);
-            }
-                        
+            } 
+            OutputStream out = response.getOutputStream();
+            ImageIO.write(bi, "png", out);
             out.close();
         }
         if (request.getParameter("lista") != null) {
             BufferedImage bi = null;
-            OutputStream out = response.getOutputStream();
             try {
-                //bi = ImageIO.read(new File(tarea1 + "Recursos/Imagenes/Listas/" + request.getParameter("lista")));
-                 img = port.getFile("Lista", request.getParameter("lista"));
-                 out.write(img);  
-            } catch (Exception e) {
+                bi = ImageIO.read(new File(tarea1 + "Recursos/Imagenes/Listas/" + request.getParameter("lista")));
+            } catch (IOException e) {
                 bi = ImageIO.read(new File(tarea1 + "Recursos/Imagenes/Listas/listaDefault.png"));
-                ImageIO.write(bi, "png", out);
-            }           
-            
+            }
+            OutputStream out = response.getOutputStream();
+            ImageIO.write(bi, "png", out);
             out.close();
         }
     }
---
+++ src/java/Services/Imagen.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import javax.jws.WebService;
+import servicios.Exception_Exception;
+
+/**
+ *
+ * @author Diego
+ */
+@WebService(serviceName = "PImagenService", portName = "PImagenPort", endpointInterface = "servicios.PImagen", targetNamespace = "http://Servicios/", wsdlLocation = "WEB-INF/wsdl/Imagen/localhost_1234/imagen.wsdl")
+public class Imagen {
+
+    public byte[] getFile(java.lang.String recurso, java.lang.String fileName) throws Exception_Exception {
+        //TODO implement this method
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public void publicar() {
+        //TODO implement this method
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+    
+}
