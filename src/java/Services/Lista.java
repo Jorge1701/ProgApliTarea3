b609f97f610fed2a3cf6107193b04bbfc5753bf0
Ale <Ale@DESKTOP-OCSH26H>
2017-11-14 18:39:14 -0300

    Merge origin/master into Tarea2
    Conflicts:
    	nbproject/project.properties

--- src/java/Services/Lista.java
+++ src/java/Services/Lista.java
@@ -1,6 +1,5 @@
 /*
-
-* To change this license header, choose License Headers in Project Properties.
+ * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -10,7 +9,7 @@ import javax.jws.WebService;
 
 /**
  *
- * @author jorge
+ * @author Ale
  */
 @WebService(serviceName = "PListaService", portName = "PListaPort", endpointInterface = "servicios.PLista", targetNamespace = "http://Servicios/", wsdlLocation = "WEB-INF/wsdl/Lista/localhost_1234/lista.wsdl")
 public class Lista {
